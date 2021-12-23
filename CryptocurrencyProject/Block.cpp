929e0eeae87231b2e04e72439fa5ab892b7125c3
iljubic <95059752+iljubic@users.noreply.github.com>
2021-12-23 21:43:14 +1000

    Update Block.cpp

--- CryptocurrencyProject/Block.cpp
+++ CryptocurrencyProject/Block.cpp
@@ -1,48 +1,31 @@
 #include "Block.h"
 
 
-Block::Block(int i, TransactionData d, std::size_t pBH) {
+Block::Block(int i, int sI, Signature s, std::string pH, std::string sH) {
 	index = i;
-	data = d;
-	prevBlockHash = pBH;
+	shardIndex = sI;
+	signature = s;
+	prevHash = pH;
+	shardHash = sH;
 
-	blockHash = generateBlockHash();
+	hash = generateHash();
 }
 
-
-std::size_t Block::generateBlockHash() {
-	// Creating hashes to be added into the finalHash
-	std::hash<std::string> hash1;
-	std::hash<std::size_t> hash2;
-	std::hash<std::size_t> hash3;
-	std::hash<std::size_t> finalHash;
-
-	// Converting TransactionData variables into hashable data
-	std::string toHash0 = std::to_string(data.amount) + std::to_string(data.timestamp);
-	std::size_t toHash1 = data.receiverAddress + data.senderAddress;
-
-	return finalHash(hash1(toHash0) + hash2(toHash1) + hash3(prevBlockHash));	// Combining all data into one hash and returning it
+// Generates a Block hash with the index, shardIndex, prevHash and shardHash that was inputted in the constructor
+std::string Block::generateHash() {
+	SHA512 sha;
+	return sha.hash(
+		std::to_string(index) + 
+		std::to_string(shardIndex) + 
+		prevHash + 
+		shardHash);
 }
 
-bool Block::isHashValid() {
-	return generateBlockHash() == blockHash;	// Returns true if the generateBlockHash() output is the same as the blockHash
+// Getters
+Signature Block::getSignature() {
+	return signature;
 }
 
-
-// Getter Functions
-int Block::getIndex()
-{
-	return index;
-}
-
-std::size_t Block::getBlockHash() {
-	return blockHash;
-}
-
-std::size_t Block::getPrevBlockHash() {
-	return prevBlockHash;
-}
-
-TransactionData Block::getTransactionData() {
-	return data;
+std::string Block::getHash() {
+	return hash;
 }
